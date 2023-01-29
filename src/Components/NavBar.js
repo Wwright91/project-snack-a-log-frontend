70fe469c4d402df19342eff2c8c9af544e0a57d3
Wwright91 <wisdomwright-martin@pursuit.org>
2023-01-29 18:49:17 -0500

    basic navbar setup

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Button, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      
+    <div id="navbar">
+      <h1 className="text-center header">Snacks App</h1>
+      <Nav fill>
+        <Nav.Item>
+            <Link to={`/snacks`}>All Snacks</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Button variant="outline-info" className="new-button">
+            <Link to={`/snacks/new`}>Add A New Snack</Link>
+          </Button>
+        </Nav.Item>
+      </Nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
