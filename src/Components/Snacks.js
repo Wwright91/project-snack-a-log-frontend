10c9196f2d16afc729b29d12b8259f2ef96e0bbd
cmm-7 <carlosmendoza@pursuit.org>
2023-01-30 21:27:44 -0400

    snacks-index-log

--- src/Components/Snacks.js
+++ src/Components/Snacks.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import Snack from "./Snack";
+
+const API = process.env.REACT_APP_API_URL;
 
 const Snacks = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [snacks, setSnacks] = useState([]);
 
-export default Snacks
+  useEffect(() => {
+    axios
+      .get(`${API}/snacks`)
+      .then((response) => {
+        console.log(response);
+        setSnacks(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="Snacks">
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Take me there</th>
+              <th>See this snack</th>
+            </tr>
+          </thead>
+          <tbody>
+            {snacks.map((snack) => {
+              return <Snack key={snack.id} snack={snack} />;
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+};
+
+export default Snacks;
--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Snacks from '../Components/Snacks'
 
 const Index = () => {
   return (
     <div>
-      
+      <h2> Index </h2>
+      <Snacks/>
     </div>
   )
 }
