553bb41de67d02e747a48c3764d15b711feeef76
Brandon Bennett <107384180+codinghashira1120@users.noreply.github.com>
2023-01-31 19:28:57 -0500

    fix

--- src/Pages/New.js
+++ src/Pages/New.js
@@ -1,12 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import SnackNewForm from "../Components/SnackNewForm";
 
 const New = () => {
   return (
     <div>
-     
+      <SnackNewForm />
     </div>
-  )
-}
+  );
+};
 
-export default New
+export default New;
