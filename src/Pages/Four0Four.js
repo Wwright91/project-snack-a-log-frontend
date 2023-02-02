cedb905279bc077e19875ff0fe8ff7ef00dadb13
Wwright91 <wisdomwright-martin@pursuit.org>
2023-02-02 14:29:54 -0500

    design for 404 page

--- src/Pages/Four0Four.js
+++ src/Pages/Four0Four.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import spaceman from "../assets/space-404.jpg";
 
 const Four0Four = () => {
   return (
-    <div>
-      <h1>Sorry, no page found</h1>
+    <div className='text-center error'>
+      <h1>Oh No, This Isn't Where We Keep The Marshmellows!</h1>
+      <h3>Return <Link className='return-home' to="/">Home</Link></h3>
+      <img src={spaceman} width='800px' height='600px'/>
     </div>
   )
 }
