6644a6d16939661d48dc0f432d9f1c5ab30d45ae
cmm-7 <carlosmendoza@pursuit.org>
2023-02-05 09:42:43 -0400

    changed path of link on home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className='Home'>
       <h1> Keep track of your most favorite snacks! </h1>
-      <Link to={"/snacks"}>
+      <Link to={"/snacks/new"}>
         <h2>  Log a snack now</h2> 
         <img src='https://www.eatthis.com/wp-content/uploads/sites/4/2017/07/fruit-nut-almond-strawberry-orange-snack-bowl.jpg' alt='health-snack' width="500px"/>
         </Link>
